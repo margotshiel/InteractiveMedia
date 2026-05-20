5b83e6df8c2182be1df2fca135a7b9bf9b7a4dd3
margotshiel <mashiel@student.unimelb.edu.au>
2026-05-20 22:08:41 +1000

    Add files via upload

--- workbook/script.js
+++ workbook/script.js
@@ -1,14 +1,34 @@
+// function windowResized(){
+//     resizeCanvas(windowWidth, windowHeight);
+// }
+
 function setup() {
-    canvas=createCanvas(windowWidth, windowHeight);
-    canvas.position(0,0);
-    canvas.style('z-index', '-1');
+    let cnv=createCanvas(windowWidth, windowHeight);
+    // canvas.position(0,0);
+    cnv.style('z-index', '-1');
+    frameRate(35);
     // background(255, 255, 255);
+    cnv.parent('p5-canvas-container');
 }
 
 function draw(){
     // background(255, 255, 255, 4);
     line(mouseX, mouseY, pmouseX, pmouseY);
-    line(width/2, height/2, mouseX, mouseY);
-    stroke(0);
-    strokeWeight(1);
+    // line(width/2, height/2, mouseX, mouseY);
+    stroke(255, 0, 0);
+    strokeWeight(5);
 }
+
+// window.onload = function() {
+//     // 1. Target the element you want to move
+//     const myBox = document.getElementById("random-box");
+    
+//     // 2. Generate random percentage values for X and Y
+//     const randomX = Math.floor(Math.random() * 80); 
+//     const randomY = Math.floor(Math.random() * 80); 
+    
+//     // 3. Apply the random positions
+//     myBox.style.position = "absolute";
+//     myBox.style.left = randomX + "%";
+//     myBox.style.top = randomY + "%";
+// };
